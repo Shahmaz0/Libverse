3b29fd2d6b816970e8b1f1c8e7f9e6c58459899f
Shahma <shahmaansari8@gmail.com>
2025-03-22 02:19:11 +0530

    Fixed: Some changes

--- LibVerse/DataModel/Book.swift
+++ LibVerse/DataModel/Book.swift
@@ -22,17 +22,3 @@ struct Book: Identifiable, Codable, Hashable {
     let publisher: String
     let imageLink: String
 }
-//
-//let sampleBooks: [Book] = [
-//    Book(title: "The Great Gatsby", author: "F. Scott Fitzgerald", description: "Nick Carraway, a young man from Minnesota, moves to New York in the summer of the 1922 to learn about the bond business.", category: "Contemporary Fiction", image: "book1"),
-//    Book(title: "To Kill a Mockingbird", author: "Harper Lee", description: "The story of a young girl and her family in the American South during the 1930s.", category: "Contemporary Fiction", image: "book2"),
-//    Book(title: "1984", author: "George Orwell", description: "A dystopian novel set in a totalitarian society ruled by the Party.", category: "Politics", image: "book3"),
-//    
-//    Book(title: "The Great Gatsby", author: "F. Scott Fitzgerald", description: "Nick Carraway, a young man from Minnesota, moves to New York in the summer of the 1922 to learn about the bond business.", category: "Contemporary Fiction", image: "book1"),
-//    Book(title: "To Kill a Mockingbird", author: "Harper Lee", description: "The story of a young girl and her family in the American South during the 1930s.", category: "Contemporary Fiction", image: "book2"),
-//    Book(title: "1984", author: "George Orwell", description: "A dystopian novel set in a totalitarian society ruled by the Party.", category: "Politics", image: "book3"),
-//]
-//
-//
-//
-//
